7249bf7613f1f0704278d26aadbcea79e10314d3
safa telli <safa.telli@gmail.com>
2017-09-21 23:51:56 -0500

    i dont know

---
+++ js/countdown.js
@@ -0,0 +1,54 @@
+
+                                        $(document).ready(function() {
+                                           
+                                          
+                                            $('#countdown4').ClassyCountdown({
+                                                 end: '1507419189',
+                                                now: Math.round(new Date().getTime()/1000),
+                                              
+                                                labels: true,
+                                                style: {
+                                                    
+                                                    element: "",
+                                                    textResponsive: .5,
+                                                    days: {
+                                                        gauge: {
+                                                            thickness: .03,
+                                                            bgColor: "rgba(255,255,255,0.05)",
+                                                            fgColor: "#1abc9c"
+                                                        },
+                                                        textCSS: 'font-family:\'Open Sans\'; font-size:25px; font-weight:300; color:#fff;'
+                                                    },
+                                                    hours: {
+                                                        gauge: {
+                                                            thickness: .03,
+                                                            bgColor: "rgba(255,255,255,0.05)",
+                                                            fgColor: "#2980b9"
+                                                        },
+                                                        textCSS: 'font-family:\'Open Sans\'; font-size:25px; font-weight:300; color:#fff;'
+                                                    },
+                                                    minutes: {
+                                                        gauge: {
+                                                            thickness: .03,
+                                                            bgColor: "rgba(255,255,255,0.05)",
+                                                            fgColor: "#8e44ad"
+                                                        },
+                                                        textCSS: 'font-family:\'Open Sans\'; font-size:25px; font-weight:300; color:#fff;'
+                                                    },
+                                                    seconds: {
+                                                        gauge: {
+                                                            thickness: .03,
+                                                            bgColor: "rgba(255,255,255,0.05)",
+                                                            fgColor: "#f39c12"
+                                                        },
+                                                        textCSS: 'font-family:\'Open Sans\'; font-size:25px; font-weight:300; color:#fff;'
+                                                    }
+
+                                                },
+                                                onEndCallback: function() {
+                                                    console.log("Time out!");
+                                                }
+                                            });
+                                          
+                                        });
+                                   
---
+++ js/hover.js
@@ -0,0 +1,9 @@
+		$("#imgs61").find("img").hover(function () {
+            $(".name>*").hide().filter($(this).data("filter")).show()
+        })
+        $("#imgs62").find("img").hover(function () {
+            $(".name>*").hide().filter($(this).data("filter")).show()
+        })
+        $("#imgs63").find("img").hover(function () {
+            $(".name>*").hide().filter($(this).data("filter")).show()
+        })
